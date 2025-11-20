5e77fd72ee2ae20f9af3193b3e0803b1569a6197
cdn-prod <alogaritmar@gmail.com>
2025-11-21 00:44:22 +0700

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-PORT = 8000
-handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print("Serving on port", PORT)
-    httpd.serve_forever()
